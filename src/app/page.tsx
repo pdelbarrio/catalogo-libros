cbefa3638696542c9d6c12ab667c7293cd083582
Pablo del Barrio <pablodbb@gmail.com>
2023-08-07 18:17:19 +0200

    refactorizado de importacion de datos para futuras importaciones desde API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 import type { Book } from "@/models/types";
-import data from "../books.json";
+
 import IndexClientPage from "./client";
 
-const books: Book[] = data.library.map((data) => data.book);
-const genres: Book["genre"][] = Array.from(
-  new Set(books.map((book) => book.genre))
-);
+const api = {
+  book: {
+    list: async (): Promise<Book[]> =>
+      import("../books.json").then((data) =>
+        data.library.map((data) => data.book)
+      ),
+  },
+};
 
-export default function IndexPage() {
+export default async function IndexPage() {
+  const books = await api.book.list();
+
+  const genres: Book["genre"][] = Array.from(
+    new Set(books.map((book) => book.genre))
+  );
   return <IndexClientPage books={books} genres={genres} />;
 }
